0a79e623daf4e6bcb7a1950c7722e0f3f5202785
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-10-10 00:15:36 -0500

    create a new user

--- index.js
+++ index.js
@@ -27,8 +27,17 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-app.post('/api/users', (req, res) => {
-
+app.post('/api/users', async (req, res) => {
+  const userObj = new User({
+    username: req.body.username
+  })
+  try {
+    const user = await userObj.save()
+    console.log(user)
+    res.json(user)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 
