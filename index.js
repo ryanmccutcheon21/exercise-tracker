6926a3f6c3d7a01435ca392c4aa2a95b2fb12fe9
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-10-10 00:45:19 -0500

    get users exercise log

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.get('/api/users', async (req, res) => {
+  const users = await User.find({}).select('_id username')
+
+  if (!users) {
+    res.send('No users')
+  } else {
+    res.json(users)
+  }
+})
+
 app.post('/api/users', async (req, res) => {
   const userObj = new User({
     username: req.body.username
@@ -70,8 +80,49 @@ app.post('/api/users/:_id/exercises', async (req, res) => {
   }
 })
 
+app.get('/api/users/:_id/logs', async (req, res) => {
+  const { from, to, limit } = req.query
+  const id = req.params._id
+  const user = await User.findById(id)
 
+  if (!user) {
+    res.send('Did not find the user')
+    return
+  } else {
+    let dateObj = {
 
+    }
+
+    if (from) {
+      dateObj['$gte'] = new Date(from)
+    } else if (to) {
+      dateObj['$lte'] = new Date(to)
+    }
+
+    let filter = {
+      user_id: id
+    }
+
+    if (from || to) {
+      filter.date = dateObj
+    }
+
+    const exercises = await Exercise.find(filter).limit(+limit ?? 500)
+
+    const log = exercises.map(exercise => ({
+      description: exercise.description,
+      duration: exercise.duration,
+      date: exercise.date.toDateString()
+    }))
+
+    res.json({
+      username: user.username,
+      count: exercises.length,
+      _id: user._id,
+      log
+    })
+  }
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
